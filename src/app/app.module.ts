177f78f0b1142134a0e7523a54c3e666f120db2b
ManuelVisi <manuel.visintin@marconirovereto.it>
2022-02-22 01:16:54 +0100

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule , RouterModule.forRoot([
+    {path: '', component: MenuComponent},
+    {path: 'about', component: AboutComponent},
+  ])],
+  declarations: [ AppComponent, HelloComponent, MenuComponent, AboutComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
